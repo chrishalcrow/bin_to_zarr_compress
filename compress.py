c238d4c61bbf3268e735ddb6daa6f8ee0b27f574
chrishalcrow <chrishalcrow@gmail.com>
2026-09-09 17:11:41 +0100

    chunk size 10k to 100k

--- compress.py
+++ compress.py
@@ -25,7 +25,7 @@ dtype = np.uint16
 num_channels = 384 
 
 shard_size = 1_000_000
-chunk_size = 10_000
+chunk_size = 100_000
 if (shard_size % chunk_size) != 0:
     raise ValueError("`chunk_size` must divide `shard_size`")
 
